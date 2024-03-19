f3136feaca2b1e819f823beb5579f9d8b976bfbd
samuelfreire03 <s.freire@uniandes.edu.co>
2024-03-19 01:12:52 -0500

    Cambios visualizacion productos

--- app/src/main/java/com/example/unimarket/ListProducts.kt
+++ app/src/main/java/com/example/unimarket/ListProducts.kt
@@ -9,19 +9,31 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.unimarket.model.Product
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.example.unimarket.data.Datasource
 import com.example.unimarket.ui.theme.UniMarketTheme
@@ -46,13 +58,13 @@ class ListProducts : ComponentActivity() {
 }
 @Composable
 fun ListProductApp() {
-    AffirmationList(
+    ProductList(
         productList = Datasource().loadAffirmations(),
     )
 }
 
 @Composable
-fun AffirmationList(productList: List<Product>, modifier: Modifier = Modifier) {
+fun ProductList(productList: List<Product>, modifier: Modifier = Modifier) {
     LazyColumn(modifier = modifier) {
         items(productList) { product ->
             AffirmationCard(
@@ -65,23 +77,49 @@ fun AffirmationList(productList: List<Product>, modifier: Modifier = Modifier) {
 
 @Composable
 fun AffirmationCard(product: Product, modifier: Modifier = Modifier) {
-    Card(modifier = modifier) {
-        Column {
+    val colors = MaterialTheme.colorScheme
+    Box(
+        modifier = modifier
+            .padding(1.dp)
+            .background(colors.surface)
+            .border(1.dp, colors.onSurface, shape = RoundedCornerShape(8.dp))
+    )
+    {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
 
-            Image(
-                painter = painterResource(product.imageResourceId),
-                contentDescription = stringResource(product.stringResourceId),
+            Box(
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .height(194.dp),
-                contentScale = ContentScale.Crop
-            )
+                    .size(200.dp)
+            ) {
 
-            Text(
-                text = LocalContext.current.getString(product.stringResourceId),
-                modifier = Modifier.padding(16.dp),
-                style = MaterialTheme.typography.headlineSmall
-            )
+                Text(
+                    text = LocalContext.current.getString(product.stringResourceId),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(Color(0xffffcfd5), shape = RoundedCornerShape(4.dp))
+                        .padding(8.dp),
+                    style = MaterialTheme.typography.bodyLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = Color(0xffff4500) // Color naranja
+                    )
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .size(200.dp)
+                    .padding(1.dp)
+            ) {
+                Image(
+                    painter = painterResource(product.imageResourceId),
+                    contentDescription = stringResource(product.stringResourceId),
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
     }
 }
