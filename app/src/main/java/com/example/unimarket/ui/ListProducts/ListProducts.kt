53f6e4ddb0f9a2a558ebf3417038876c48f72fb0
samuelfreire03 <s.freire@uniandes.edu.co>
2024-03-20 13:21:40 -0500

    cambios de imports

--- app/src/main/java/com/example/unimarket/ui/ListProducts/ListProducts.kt
+++ app/src/main/java/com/example/unimarket/ui/ListProducts/ListProducts.kt
@@ -1,7 +1,6 @@
 package com.example.unimarket.ui.ListProducts
 
 import android.content.res.Configuration
-import android.widget.Toast
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.example.unimarket.model.Product
@@ -12,20 +11,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.aspectRatio
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.DockedSearchBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconToggleButton
@@ -40,9 +33,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -56,12 +47,10 @@ import com.example.unimarket.data.Datasource
 @Composable
 fun ListProductApp(modifier: Modifier = Modifier) {
     val productList = remember { Datasource().loadAffirmations() }
-    val ctx = LocalContext.current
     var query by remember { mutableStateOf("") }
-    var active by remember { mutableStateOf(true) } // Mantén la barra de búsqueda activa todo el tiempo
+    var active by remember { mutableStateOf(true) }
 
     Column(modifier = modifier.fillMaxSize()) {
-        // Barra de búsqueda
         SearchBar(
             query = query,
             onQueryChange = { query = it },
@@ -75,11 +64,11 @@ fun ListProductApp(modifier: Modifier = Modifier) {
                 productList = productList.filter { product ->
                     product.stringResourceId.contains(query, ignoreCase = true)
                 },
-                modifier = Modifier.weight(1f) // Permitir que la lista ocupe todo el espacio disponible
+                modifier = Modifier.weight(1f)
             )
 
             Button(
-                onClick = { /* Acción del botón */ },
+                onClick = { },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(16.dp)
