28fb203f4508bb30f8a0b6a652cdecef52799a62
msgalvan1207 <ms.galvan@uniandes.edu.co>
2024-04-23 10:06:08 -0500

    experiment with lifecycle and onDestroy() method of main Activity
    the idea is that in the onDestroy() method of the activity, it should persist the contents of shopping cart. This method is called when the activity is destroyed. If this doesn't work. Consider using the same code on onPause().
    Furthermore, to not block the app closing down, I'm trying to use lifecycle scope and coroutines

--- app/src/main/java/com/example/unimarket/MainActivity.kt
+++ app/src/main/java/com/example/unimarket/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.unimarket
 
 import android.content.pm.ActivityInfo
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
@@ -12,6 +13,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.unimarket.ui.Login.model.LoginModel
 import com.example.unimarket.ui.Login.ui.UserInfoScreen
@@ -22,6 +24,9 @@ import com.example.unimarket.ui.camera.ui.LightSensorViewModel
 import com.example.unimarket.ui.navigation.Nav
 import com.example.unimarket.ui.theme.UniMarketTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -50,6 +55,17 @@ class MainActivity : ComponentActivity() {
                     //UserInfoScreen(viewModel = UserInfoViewModel(LoginModel()))
                     Nav(lightSensorViewModel)
                     //Greeting("Android")
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        for (i in 1 until 10) {
+            lifecycleScope.launch{
+                withContext(Dispatchers.Default) {
+                    Log.d("On Destroy", "$i")
                 }
             }
         }
